2be4b9ba9736bbb61726050e256063644fe4b14c
taka <117965802+taka012345@users.noreply.github.com>
2024-11-26 22:47:00 +0900

    Add files via upload

--- _site/js/memoMenu.js
+++ _site/js/memoMenu.js
@@ -1,17 +1,13 @@
-document.addEventListener("DOMContentLoaded", function() {
-    const memoToggle = document.getElementById("memo-toggle");
-    const memoMenu = document.getElementById("memo-menu");
-  
-    memoToggle.addEventListener("click", function(event) {
-      event.preventDefault();
-      memoMenu.style.display = memoMenu.style.display === 'none' ? 'block' : 'none';
-      event.stopPropagation();
-    });
-  
-    document.addEventListener("click", function(event) {
-      if (!memoMenu.contains(event.target) && !memoToggle.contains(event.target)) {
-        memoMenu.style.display = 'none';
-      }
-    });
-  });
-  
+document.getElementById('memo-toggle').onclick = function(event) {
+  event.stopPropagation(); // 他の場所をクリック時に閉じるための対策
+  var menu = document.getElementById('memo-menu');
+  menu.style.display = menu.style.display === 'none' ? 'block' : 'none';
+};
+
+// 画面の他の場所をクリックしたらメニューを閉じる
+document.addEventListener('click', function(event) {
+  var menu = document.getElementById('memo-menu');
+  if (!menu.contains(event.target) && event.target.id !== 'memo-toggle') {
+    menu.style.display = 'none';
+  }
+});
